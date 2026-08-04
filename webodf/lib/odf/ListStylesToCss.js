514925235b831181a5fb7d84456c3f595011b6fd
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-08-04 00:00:00 +0000

    Named who wrote what was written anew in these files.

--- webodf/lib/odf/ListStylesToCss.js
+++ webodf/lib/odf/ListStylesToCss.js
@@ -1,5 +1,6 @@
 /**
  * Copyright (C) 2010-2014 KO GmbH <copyright@kogmbh.com>
+ * Copyright (C) 2026 Daniel Berthereau <Daniel.git@Berthereau.net>
  *
  * @licstart
  * This file is part of WebODF.
